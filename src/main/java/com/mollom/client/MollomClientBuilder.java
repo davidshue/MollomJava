56210e815fe6a8fb10dd6e8b5925216c6f8044b9
sun <sun@unleashedmind.com>
2013-11-02 12:54:55 +0100

    Fixed README examples should not suggest to build/destroy a new MollomClient for every request.

--- src/main/java/com/mollom/client/MollomClientBuilder.java
+++ src/main/java/com/mollom/client/MollomClientBuilder.java
@@ -13,7 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
 /**
- * Builds a new mollomClient instance to interact with the Mollom service.
+ * Builds a new MollomClient instance to interact with the Mollom service.
  *
  * The initial entry point for the Mollom API library.
  */
